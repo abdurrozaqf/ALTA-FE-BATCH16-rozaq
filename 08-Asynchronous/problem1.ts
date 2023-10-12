50c36aec0bbd3a49f309e3363c34dcb222a305da
abdurrozaqf <rozaqa27@gmail.com>
2023-10-12 18:52:32 +0700

    feat: finish task for part 8

--- 08-Asynchronous/problem1.ts
+++ 08-Asynchronous/problem1.ts
@@ -1,9 +1,32 @@
 function jajanBoba(uang: number, callback: (uang: number) => void): void {
   // your code here
+
+  const priceBoba = 5000;
+
+  setTimeout(() => {
+    console.log(`kamu jajan boba dengan harga Rp.${priceBoba}`);
+    uang -= priceBoba;
+    callback(uang);
+    console.log(`sisa uang kamu Rp.${uang}`);
+  }, 5000);
 }
 
 function jajanSeblak(uang: number): void {
   // your code here
+
+  const priceSeblak = 8000;
+  if (uang >= priceSeblak) {
+    setTimeout(() => {
+      console.log(`kamu jajan seblak dengan harga Rp.${priceSeblak}`);
+      uang -= priceSeblak;
+      console.log(`sisa uang kamu Rp.${uang}`);
+    }, 9000);
+  } else {
+    setTimeout(() => {
+      console.log("Maaf uang kamu belum cukup untuk membeli Seblak");
+      console.log(`sisa uang kamu Rp.${uang}`);
+    }, 2000);
+  }
 }
 
 jajanBoba(20000, jajanSeblak);
--- 08-Asynchronous/problem3.ts
+++ 08-Asynchronous/problem3.ts
@@ -1,5 +1,19 @@
 function lottery(num: number): Promise<string> {
   // your code here
+
+  return new Promise<string>((resolve, reject) => {
+    console.log("undian lotre dimulai...");
+    console.log("sedang mengundi nomor anda...");
+    let rate = +(Math.random() * 1000).toFixed();
+
+    setTimeout(() => {
+      if (num === rate) {
+        resolve("selamat anda mendapatkan hadiah utama berupa mobil");
+      } else {
+        reject("maaf anda kurang beruntung");
+      }
+    }, 2000);
+  });
 }
 
 lottery(5)
