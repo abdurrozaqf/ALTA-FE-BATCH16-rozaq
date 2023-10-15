58dfc2611e0a9e838fda1774501299648dfc675c
abdurrozaqf <rozaqa27@gmail.com>
2023-10-15 10:57:03 +0700

    feat: finish task typescript challenge part 1

--- 11-TypeScript-Challenge-1/problem1.ts
+++ 11-TypeScript-Challenge-1/problem1.ts
@@ -1,5 +1,18 @@
-function playWithAsterik(n: number): void {
+function playWithAsterik(n: number): void | any {
   // your code here
+
+  let result = "";
+  for (let index = 1; index <= n; index++) {
+    for (let j = 1; j <= n - index; j++) {
+      result += " ";
+    }
+    for (let k = 1; k <= index; k++) {
+      result += "* ";
+    }
+
+    result += "\n";
+  }
+  return result;
 }
 
 console.log(playWithAsterik(5));
