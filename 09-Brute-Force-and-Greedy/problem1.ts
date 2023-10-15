eeaae60ef352e43c327b06fc5a39b9c1d71aa9e7
abdurrozaqf <rozaqa27@gmail.com>
2023-10-15 11:37:08 +0700

    feat: finish task for part 9

--- 09-Brute-Force-and-Greedy/problem1.ts
+++ 09-Brute-Force-and-Greedy/problem1.ts
@@ -1,5 +1,19 @@
 function simpleEquations(a: number, b: number, c: number): string {
   // your code here
+
+  let result: string = "";
+  for (let x = 1; x <= a; x++) {
+    for (let y = 1; y <= a; y++) {
+      for (let z = 1; z <= a; z++) {
+        if (x + y + z === a && x * y * z === b && x * x + y * y + z * z === c) {
+          return `${x} ${y} ${z}`;
+        } else {
+          result = "no solution";
+        }
+      }
+    }
+  }
+  return result;
 }
 
 console.log(simpleEquations(1, 2, 3)); // no solution
