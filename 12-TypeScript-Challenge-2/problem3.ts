a9693cc5b0e98111fbe8efa4fe91c8a34bd51086
abdurrozaqf <rozaqa27@gmail.com>
2023-10-20 22:03:46 +0700

    feat: finish task typescript challenge part 2

--- 12-TypeScript-Challenge-2/problem3.ts
+++ 12-TypeScript-Challenge-2/problem3.ts
@@ -1,5 +1,24 @@
 function makeDiamond(char: string): void {
   // your code here
+
+  const alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  let indexAlphabet = alphabet.indexOf(char);
+
+  let result: string[] = [];
+  for (let i = 0; i <= indexAlphabet; i++) {
+    let dot = "•".repeat(indexAlphabet - i) + alphabet[i];
+    if (i > 0) {
+      dot += "•".repeat(2 * i - 1);
+      dot += alphabet[i];
+    }
+    dot += "•".repeat(indexAlphabet - i);
+    result.push(dot);
+  }
+  for (let i = indexAlphabet - 1; i >= 0; i--) {
+    result.push(result[i]);
+  }
+
+  console.log(result.join("\n") + "\n");
 }
 
 makeDiamond("C");
