c3526c5df0d6677251f52ea601ed124e902c5713
abdurrozaqf <rozaqa27@gmail.com>
2023-10-20 14:53:30 +0700

    feat: finish task typescript challenge part 2

--- 12-TypeScript-Challenge-2/problem4.ts
+++ 12-TypeScript-Challenge-2/problem4.ts
@@ -1,5 +1,18 @@
 function findMaxSumSubArray(k: number, arr: number[]): number {
   // your code here
+
+  let result = 0;
+  const tempSum = [];
+  for (let i = 0; i < arr.length - 1; i++) {
+    const dupiclateForSum = arr.slice(i, i + k);
+    const sumArray = dupiclateForSum.reduce((a, b) => a + b, 0);
+    tempSum.push(sumArray);
+
+    const maxSumValue = Math.max(...tempSum);
+    result = maxSumValue;
+  }
+
+  return result;
 }
 
 console.log(findMaxSumSubArray(3, [2, 1, 5, 1, 3, 2])); // 9
