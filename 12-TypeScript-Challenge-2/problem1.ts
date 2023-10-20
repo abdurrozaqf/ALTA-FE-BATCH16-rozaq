bc8f3a3f7068142802edf8eccd118a493ff5df1b
abdurrozaqf <rozaqa27@gmail.com>
2023-10-20 13:26:26 +0700

    feat: finish task typescript challenge part 2

--- 12-TypeScript-Challenge-2/problem1.ts
+++ 12-TypeScript-Challenge-2/problem1.ts
@@ -1,5 +1,22 @@
 function pairSum(arr: number[], target: number): number[] {
   // your code here
+  const len = arr.length;
+
+  let tempSum: number[] = [];
+  for (let i = 0; i < len; i++) {
+    for (let j = 0; j < len; j++) {
+      const sum = arr[i] + arr[j];
+      if (sum == target) {
+        tempSum.push(arr[i]);
+        tempSum.push(arr[j]);
+      }
+    }
+  }
+  let result: number[] = [];
+  result.push(arr.indexOf(tempSum[0]));
+  result.push(arr.indexOf(tempSum[1]));
+
+  return result;
 }
 
 console.log(pairSum([1, 2, 3, 4, 6], 6)); // [1, 3]
